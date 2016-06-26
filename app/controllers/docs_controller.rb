ee73065a4d808abe63e130597c2fc3fdb6926370
Brian Donahue <bpd4tw@virginia.edu>
2016-06-26 18:23:39 -0400

    Add docs views and define actions

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,6 +1,9 @@
 class DocsController < ApplicationController
+before_action :find_doc, only: [:show, :edit, :update, :destory]
+
 
   def index
+    @docs = Doc.all.order("created_at DESC")
   end
 
   def show
@@ -31,6 +34,7 @@ class DocsController < ApplicationController
 
   private
     def find_doc
+      @doc = Doc.find(params[:id])
     end
 
     def doc_params
